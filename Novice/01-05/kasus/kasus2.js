07f0af1aacbe58dff3476c69db62f5a7ca1913d5
Rahagita <byrahagita@gmail.com>
2019-12-09 16:12:06 +0700

    praxis-academy

---
+++ Novice/01-05/kasus/kasus2.js
@@ -0,0 +1,12 @@
+let kosakata = new Map([
+  ['cucumber', 'imun'],
+  ['tomatoes', 'tomat'],
+  ['onion',    'bawang']])
+// console.log('cucumber');
+
+function cariKata(){
+    document.write(kosakata.get('cucumber'))
+
+}
+
+cariKata()
---
+++ Novice/01-05/latihan/object.js
@@ -0,0 +1,27 @@
+// let user = {};
+// user.name = 'jhon';
+// user.surname = 'Smith';
+// user.name = 'Pate';
+// delete user.name;
+
+// check for empetiness
+// let schedule = {};
+//
+// alert( isEmpty(schedule) ); // true
+//
+// schedule["8:30"] = "get up";
+//
+// alert( isEmpty(schedule) ); // false
+
+// constant object
+const user = {
+  name: "John"
+};
+
+// // works
+// user.name = "Pete";
+//
+// // error
+user = 123;
+
+console.log(user);
